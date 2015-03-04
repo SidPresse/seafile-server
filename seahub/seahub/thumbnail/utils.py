fefce1d4b85117bc1751884262950f8a64fcc488
https://github.com/s-long <slong@lesechos.fr>
2015-03-04 16:16:28 +0100

    seafile-server-4.0.6_x86-64

---
+++ seahub/seahub/thumbnail/utils.py
@@ -0,0 +1,5 @@
+import posixpath
+from seahub.utils import get_service_url
+def get_thumbnail_src(repo_id, obj_id, size):
+    return posixpath.join(get_service_url(), "thumbnail", repo_id,
+                          obj_id, size)
