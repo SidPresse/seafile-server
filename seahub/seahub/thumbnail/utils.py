e36fd556fa273e2abefaf1bdef02f99e6c587bdc
https://github.com/s-long <slong@lesechos.fr>
2015-11-12 13:14:31 +0100

    Une version stable. 1er génération cloud-expertinfos

--- seahub/seahub/thumbnail/utils.py
+++
@@ -1,5 +0,0 @@
-import posixpath
-from seahub.utils import get_service_url
-def get_thumbnail_src(repo_id, obj_id, size):
-    return posixpath.join(get_service_url(), "thumbnail", repo_id,
-                          obj_id, size)
